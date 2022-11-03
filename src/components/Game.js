e68e5c76291f6fb8f86ed45c30d460d1a2948c5b
Jun Ou <55570492+junou1324@users.noreply.github.com>
2022-11-04 00:38:45 +1100

    Added game.scss and styling for game page.
    Added icons to the game page for both player and house.
    
    Added dynamically showing shadow circles behind winner icon

--- src/components/Game.js
+++ src/components/Game.js
@@ -48,8 +48,27 @@ useEffect(()=>{
     <div className="game">
       <div className="game__you">
         <span className="text">You Picked</span>
-        <div className="icon icon--paper"></div>
+        <div className={`icon icon--${myChoice} ${playerWin=="win"?`icon icon--${myChoice}--winner`:''}`}></div>
       </div>
+
+      {playerWin=="win" && <div className="game__play">
+        <span className="text">You Win</span>
+        <Link to="/" className="play-again"onClick={()=>setHouse()}>Play Again</Link>
+      </div>}
+      {playerWin=="lose" && <div className="game__play">
+        <span className="text">You lose</span>
+        <Link to="/" className="play-again"onClick={()=>setHouse()}>Play Again</Link>
+      </div>}
+      {playerWin=="draw" && <div className="game__play">
+        <span className="text">Draw</span>
+        <Link to="/" className="play-again"onClick={()=>setHouse()}>Play Again</Link>
+      </div>}
+
+      <div className="game__house">
+        <span className="text">The House Picked</span>
+        <div className={`icon icon--${house} ${playerWin=="lose"?`icon icon--${house }--winner`:''}`}></div>
+      </div>
+      
     </div>
   )
 }
